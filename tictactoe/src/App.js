9b438f37215260d0ba266bd918ba712557079291
teresa0605 <teresawan2018@gmail.com>
2023-04-06 16:01:30 -0400

    Implemented core features

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const winningCombinations = [
+  [0, 1, 2], // Top row
+  [3, 4, 5], // Middle row
+  [6, 7, 8], // Bottom row
+  [0, 3, 6], // Left column
+  [1, 4, 7], // Middle column
+  [2, 5, 8], // Right column
+  [0, 4, 8], // Diagonal from top-left to bottom-right
+  [2, 4, 6] // Diagonal from top-right to bottom-left
+];
+
+const initialBoard = Array(9).fill(null);
+
+const App = () => {
+  const [board, setBoard] = useState(initialBoard);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+  const [winner, setWinner] = useState(null);
+  const [isDraw, setIsDraw] = useState(false);
+
+  const handleCellClick = (index) => {
+    if (board[index] || winner) return; // Ignore clicks on occupied cells or after the game is won
+    const newBoard = [...board];
+    newBoard[index] = currentPlayer;
+    setBoard(newBoard);
+    checkForWin(newBoard);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const checkForWin = (board) => {
+    for (let combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+        setWinner(board[a]);
+        return;
+      }
+    }
+    if (!board.includes(null)) {
+      setIsDraw(true);
+    }
+  };
+
+  const resetGame = () => {
+    setBoard(initialBoard);
+    setCurrentPlayer("X");
+    setWinner(null);
+    setIsDraw(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tic-Tac-Toe</h1>
+      <div className="game-board">
+        {board.map((cell, index) => (
+          <div
+            key={index}
+            className="cell"
+            onClick={() => handleCellClick(index)}
+          >
+            {cell}
+          </div>
+        ))}
+      </div>
+      {winner && <p className="message">{`${winner} wins!`}</p>}
+      {isDraw && <p className="message">It's a draw!</p>}
+      {!winner && !isDraw && (
+        <p className="current-player">{`Current player: ${currentPlayer}`}</p>
+      )}
+      <button className="reset-button" onClick={resetGame}>
+        Reset Game
+      </button>
     </div>
   );
-}
+};
 
 export default App;
